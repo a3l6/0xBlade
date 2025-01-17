6619976afa05a9e2e3fd4a33aa9a8339158e5e53
Emilio Mendoza <ironislife39@gmail.com>
2025-01-17 11:58:39 -0500

    More work on printing and rendering

--- src/level.go
+++ src/level.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 type Level struct {
 	sprite []string
@@ -20,25 +23,50 @@ const Grenade_ID uint8 = 2
 const Enemy_ID uint8 = 3
 
 type LevelChar struct {
-	char uint8
-	id   uint8
+	char   uint8 // ASCII value
+	master uint8
 }
 
-func (l *Level) print(char LevelChar, x uint8, y uint8) {
-	l.level[y][x] = char
+// Sets LevelChar at some coordinate. Does not render.
+func (l *Level) print(master uint8, char uint8, x uint8, y uint8) error {
+	// Bounds check
+	if x > l.width || y > l.height {
+		return errors.New("x or y value is out of bounds")
+	}
+
+	l.level[y][x] = LevelChar{char: char, master: master}
+	return nil
 }
 
-func (l *Level) println(chars string, x uint8, y uint8) {
+// Sets line of LevelChars. Does not render.
+func (l *Level) println(master uint8, chars string, y uint8) error {
+
+	// Checking for max uint8
+	if y > 255 {
+		return errors.New("x or y value is too big")
+	}
+	if len(chars) != int(l.width) {
+		return errors.New("string value needs to be the same as the level width")
+	}
+
 	var output []LevelChar
+	for _, val := range chars {
+		char := uint8(val)
+		lc := LevelChar{char: char, master: master}
+		output = append(output, lc)
+	}
 	l.level[y] = output
+	return nil
 }
 
+// Converts ALL rows to strings and prints in parallel.
 func (l *Level) render() {
 
 }
 
-func (l *Level) renderln() {
-
+// Converts ONE row to a string and returns
+func (l *Level) renderln() string {
+	return ""
 }
 
 func (l *Level) draw() {
