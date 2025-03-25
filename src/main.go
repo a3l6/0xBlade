ed319cb01b93c178e920b1188a21ccde7cfe40fb
Emilio Mendoza <ironislife39@gmail.com>
2025-03-25 11:33:19 -0400

    Added support for more buttons, programatically

--- src/main.go
+++ src/main.go
@@ -29,6 +29,8 @@ var level *Level = &Level{
 	leftBound:  60,
 }
 
+var mode string = "menu"
+
 func main() {
 	oldState, err := term.MakeRaw(int(os.Stdin.Fd()))
 	if err != nil {
@@ -43,156 +45,199 @@ func main() {
 		term.Restore(int(os.Stdin.Fd()), oldState)
 		os.Exit(0)
 	}()
-
-	mode := "menu"
-
-	switch mode {
-	case "menu":
-		fmt.Printf("\033[2J")
-		width, height, err := term.GetSize(int(os.Stderr.Fd()))
-		if err != nil {
-			width := windowWidth
-			height := windowHeight
-			fmt.Printf("Error: %d %d", width, height)
-		}
-
-		emptySpace := []rune(strings.Repeat(string([]rune{' ', ' '}), 40)) // These are spaces
-		currBuffer := make([]rune, width*height)
-		prevBuffer := make([]rune, width*height)
-		splash_screen := strings.Split(
-			"▒█████  ▒██   ██▒ ▄▄▄▄    ██▓    ▄▄▄      ▓█████▄ ▓█████\r\n"+
-				"▒██▒  ██▒▒▒ █ █ ▒░▓█████▄ ▓██▒   ▒████▄    ▒██▀ ██▌▓█   ▀ \r\n"+
-				"▒██░  ██▒░░  █   ░▒██▒ ▄██▒██░   ▒██  ▀█▄  ░██   █▌▒███   \r\n"+
-				"▒██   ██░ ░ █ █ ▒ ▒██░█▀  ▒██░   ░██▄▄▄▄██ ░▓█▄   ▌▒▓█  ▄ \r\n"+
-				"░ ████▓▒░▒██▒ ▒██▒░▓█  ▀█▓░██████▒▓█   ▓██▒░▒████▓ ░▒████▒\r\n"+
-				"░ ▒░▒░▒░ ▒▒ ░ ░▓ ░░▒▓███▀▒░ ▒░▓  ░▒▒   ▓▒█░ ▒▒▓  ▒ ░░ ▒░ ░\r\n"+
-				"  ░ ▒ ▒░ ░░   ░▒ ░▒░▒   ░ ░ ░ ▒  ░ ▒   ▒▒ ░ ░ ▒  ▒  ░ ░  ░\r\n"+
-				"░ ░ ░ ▒   ░    ░   ░    ░   ░ ░    ░   ▒    ░ ░  ░    ░   \r\n"+
-				"    ░ ░   ░    ░   ░          ░  ░     ░  ░   ░       ░  ░\r\n"+
-				"                        ░                   ░              \r\n",
-			"\r\n")
-
-		var buttons [1]libtui.Button
-		buttons[0] = libtui.Button{
-			Width:  20,
-			Height: 1,
-			Align:  libtui.AlignLeft,
-			Value:  "Start Game       s",
-			Key:    's',
-			Callback: func() {
-				mode = "main"
-			},
-		}
-
-		const fps = 10
-		const frameDuration = time.Second / fps
-		buf := make([]byte, 1)
-
-		handleInputs := func() {
-			frameDuration := time.Second / 120
-			for {
-				start := time.Now()
-				_, err := os.Stdin.Read(buf)
-				if err != nil {
-					term.Restore(int(os.Stdin.Fd()), oldState)
-
-					log.Fatal(err)
-					panic(err)
-				}
-				elapsed := time.Since(start)
-				sleepTime := frameDuration - elapsed
-				if sleepTime > 0 {
-					time.Sleep(sleepTime)
-				}
-			}
-
-		}
-		go handleInputs()
-
-		for {
-			start := time.Now()
-			copy(currBuffer[:], emptySpace[:])
-
-			for idx, row := range splash_screen {
-				copy(currBuffer[width*idx:], []rune(row))
-			}
-
-			btn_rendered, err := buttons[0].RenderToArrRunes()
+	for {
+		switch mode {
+		case "menu":
+			fmt.Printf("\033[2J")
+			width, height, err := term.GetSize(int(os.Stderr.Fd()))
 			if err != nil {
-				log.Fatal(err)
+				width := windowWidth
+				height := windowHeight
+				fmt.Printf("Error: %d %d", width, height)
 			}
 
-			for range buttons {
-				copy(currBuffer[width*25:], []rune{rune(buf[0])}) //[]rune(fmt.Sprintf("%#v", buttons[0])))
-				if buf[0] == 's' {
-					mode = "main" //btn.Callback()
-				}
+			emptySpace := []rune(strings.Repeat(string([]rune{' ', ' '}), 40)) // These are spaces
+			currBuffer := make([]rune, width*height)
+			prevBuffer := make([]rune, width*height)
+			splash_screen := strings.Split(
+				"▒█████  ▒██   ██▒ ▄▄▄▄    ██▓    ▄▄▄      ▓█████▄ ▓█████\r\n"+
+					"▒██▒  ██▒▒▒ █ █ ▒░▓█████▄ ▓██▒   ▒████▄    ▒██▀ ██▌▓█   ▀ \r\n"+
+					"▒██░  ██▒░░  █   ░▒██▒ ▄██▒██░   ▒██  ▀█▄  ░██   █▌▒███   \r\n"+
+					"▒██   ██░ ░ █ █ ▒ ▒██░█▀  ▒██░   ░██▄▄▄▄██ ░▓█▄   ▌▒▓█  ▄ \r\n"+
+					"░ ████▓▒░▒██▒ ▒██▒░▓█  ▀█▓░██████▒▓█   ▓██▒░▒████▓ ░▒████▒\r\n"+
+					"░ ▒░▒░▒░ ▒▒ ░ ░▓ ░░▒▓███▀▒░ ▒░▓  ░▒▒   ▓▒█░ ▒▒▓  ▒ ░░ ▒░ ░\r\n"+
+					"  ░ ▒ ▒░ ░░   ░▒ ░▒░▒   ░ ░ ░ ▒  ░ ▒   ▒▒ ░ ░ ▒  ▒  ░ ░  ░\r\n"+
+					"░ ░ ░ ▒   ░    ░   ░    ░   ░ ░    ░   ▒    ░ ░  ░    ░   \r\n"+
+					"    ░ ░   ░    ░   ░          ░  ░     ░  ░   ░       ░  ░\r\n"+
+					"                        ░                   ░              \r\n",
+				"\r\n")
+
+			var buttons [2]libtui.Button
+			buttons[0] = libtui.Button{
+				Width:  20,
+				Height: 1,
+				Align:  libtui.AlignLeft,
+				Value:  "Start Game       s",
+				Key:    's',
+				Callback: func() {
+					mode = "main"
+				},
+			}
+			buttons[1] = libtui.Button{
+				Width:    20,
+				Height:   1,
+				Align:    libtui.AlignLeft,
+				Value:    "Quit             q",
+				Key:      'q',
+				Callback: func() {},
 			}
 
-			if buf[0] == 'q' {
-				return
-			}
+			const fps = 10
+			const frameDuration = time.Second / fps
+			buf := make([]byte, 1)
 
-			copy(currBuffer[width*20:], btn_rendered[:])
-
-			for i := range currBuffer {
-				if currBuffer[i] != prevBuffer[i] {
-					x, y := i%windowWidth, i/windowWidth
-					fmt.Printf("\033[%d;%dH%c", y+1, x+1, currBuffer[i])
-				}
-			}
-			copy(prevBuffer[:], currBuffer[:])
-
-			elapsed := time.Since(start)
-			sleepTime := frameDuration - elapsed
-			if sleepTime > 0 {
-				time.Sleep(sleepTime)
-			}
-		}
-
-	case "main":
-
-		buf := make([]byte, 1)
-
-		player := &Player{pos: Vector2{61, 2}, sprite: "&", keymap: keymap}
-
-		handleInputs := func() {
-			frameDuration := time.Second / 120
-			for {
-				start := time.Now()
-				_, err := os.Stdin.Read(buf)
-				if err != nil {
-					term.Restore(int(os.Stdin.Fd()), oldState)
-
-					log.Fatal(err)
-					panic(err)
-				}
-				player.move(buf)
-
-				//gameManager.console["buf"] = "\"" + string(buf) + "\""
-				gameManager.writeToConsole("buf", fmt.Sprintf("\"%s\"", string(buf)))
-				if buf[0] == ' ' {
-					err = gameManager.createNewGrenade(player.pos, player.last_direction)
+			handleInputs := func() {
+				frameDuration := time.Second / 120
+				for {
+					start := time.Now()
+					_, err := os.Stdin.Read(buf)
 					if err != nil {
+						term.Restore(int(os.Stdin.Fd()), oldState)
+
+						log.Fatal(err)
+						panic(err)
 					}
+					elapsed := time.Since(start)
+					sleepTime := frameDuration - elapsed
+					if sleepTime > 0 {
+						time.Sleep(sleepTime)
+					}
+					if mode != "menu" {
+						break
+					}
+
 				}
 
-				elapsed := time.Since(start)
-				sleepTime := frameDuration - elapsed
-				if sleepTime > 0 {
-					time.Sleep(sleepTime)
-				}
 			}
+			go handleInputs()
 
-		}
-
-		// Needs to run faster to update all the time
-		go func() {
-			frameDuration := time.Second / 120
 			for {
 				start := time.Now()
+				copy(currBuffer[:], emptySpace[:])
 
-				gameManager.drawScreen()
+				for idx, row := range splash_screen {
+					copy(currBuffer[width*idx:], []rune(row))
+				}
+
+				for _, btn := range buttons {
+					copy(currBuffer[width*25:], []rune{rune(buf[0])})
+					if buf[0] == byte(btn.Key) {
+						btn.Callback()
+					}
+				}
+
+				if buf[0] == 'q' {
+					return
+				}
+
+				btn_rendered, err := buttons[0].RenderToArrRunes()
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				copy(currBuffer[width*20:], btn_rendered[:])
+
+				for i := range currBuffer {
+					if currBuffer[i] != prevBuffer[i] {
+						x, y := i%windowWidth, i/windowWidth
+						fmt.Printf("\033[%d;%dH%c", y+1, x+1, currBuffer[i])
+					}
+				}
+				copy(prevBuffer[:], currBuffer[:])
+
+				elapsed := time.Since(start)
+				sleepTime := frameDuration - elapsed
+				if sleepTime > 0 {
+					time.Sleep(sleepTime)
+				}
+
+				if mode != "menu" {
+					break
+				}
+			}
+
+		case "main":
+
+			buf := make([]byte, 1)
+
+			player := &Player{pos: Vector2{61, 2}, sprite: "&", keymap: keymap}
+
+			handleInputs := func() {
+				frameDuration := time.Second / 120
+				for {
+					start := time.Now()
+					_, err := os.Stdin.Read(buf)
+					if err != nil {
+						term.Restore(int(os.Stdin.Fd()), oldState)
+
+						log.Fatal(err)
+						panic(err)
+					}
+					player.move(buf)
+
+					//gameManager.console["buf"] = "\"" + string(buf) + "\""
+					gameManager.writeToConsole("buf", fmt.Sprintf("\"%s\"", string(buf)))
+					if buf[0] == ' ' {
+						err = gameManager.createNewGrenade(player.pos, player.last_direction)
+						if err != nil {
+						}
+					}
+
+					elapsed := time.Since(start)
+					sleepTime := frameDuration - elapsed
+					if sleepTime > 0 {
+						time.Sleep(sleepTime)
+					}
+				}
+
+			}
+
+			// Needs to run faster to update all the time
+			go func() {
+				frameDuration := time.Second / 120
+				for {
+					start := time.Now()
+
+					gameManager.drawScreen()
+
+					elapsed := time.Since(start)
+					sleepTime := frameDuration - elapsed
+					if sleepTime > 0 {
+						time.Sleep(sleepTime)
+					}
+				}
+			}()
+
+			//level.id = gameManager.registerAsObject(level)
+			player.id = gameManager.registerAsObject(player)
+
+			gameManager.createNewEnemy(Vector2{70, 10})
+
+			const fps = 10
+			const frameDuration = time.Second / fps
+
+			go handleInputs()
+
+			for {
+				start := time.Now()
+				// TODO: convert to once??
+				gameManager.ptrPlayer = player
+
+				gameManager.StepAll()
+
+				if buf[0] == 'q' {
+					return
+				}
 
 				elapsed := time.Since(start)
 				sleepTime := frameDuration - elapsed
@@ -200,119 +245,91 @@ func main() {
 					time.Sleep(sleepTime)
 				}
 			}
-		}()
 
-		//level.id = gameManager.registerAsObject(level)
-		player.id = gameManager.registerAsObject(player)
+		case "level_editor": // I dont need this
+			fmt.Printf("\033[2J\033[H")
 
-		gameManager.createNewEnemy(Vector2{70, 10})
-
-		const fps = 10
-		const frameDuration = time.Second / fps
-
-		go handleInputs()
-
-		for {
-			start := time.Now()
-			// TODO: convert to once??
-			gameManager.ptrPlayer = player
-
-			gameManager.StepAll()
-
-			if buf[0] == 'q' {
-				return
+			var level []string = make([]string, 45)
+			for idx := range level {
+				level[idx] = strings.Repeat("# ", 95) // 95x47 is how much it takes for the whole screen on my laptop
 			}
 
-			elapsed := time.Since(start)
-			sleepTime := frameDuration - elapsed
-			if sleepTime > 0 {
-				time.Sleep(sleepTime)
-			}
-		}
+			drawSquare := func(lev *[]string, replacementChar string, trueSquare bool, radius int) {
+				radius = Abs(radius)
+				centerx, centery := 80, 22
 
-	case "level_editor": // I dont need this
-		fmt.Printf("\033[2J\033[H")
-
-		var level []string = make([]string, 45)
-		for idx := range level {
-			level[idx] = strings.Repeat("# ", 95) // 95x47 is how much it takes for the whole screen on my laptop
-		}
-
-		drawSquare := func(lev *[]string, replacementChar string, trueSquare bool, radius int) {
-			radius = Abs(radius)
-			centerx, centery := 80, 22
-
-			for idx, val := range level {
-				if centery-radius <= idx && centery+radius >= idx {
-					left, right := centerx-radius, centerx+radius
-					if trueSquare {
-						(*lev)[idx] = val[0:left] + strings.Repeat(replacementChar, radius) + val[right:]
-					} else {
-						(*lev)[idx] = val[0:left] + strings.Repeat(replacementChar, 2*radius) + val[right:]
+				for idx, val := range level {
+					if centery-radius <= idx && centery+radius >= idx {
+						left, right := centerx-radius, centerx+radius
+						if trueSquare {
+							(*lev)[idx] = val[0:left] + strings.Repeat(replacementChar, radius) + val[right:]
+						} else {
+							(*lev)[idx] = val[0:left] + strings.Repeat(replacementChar, 2*radius) + val[right:]
+						}
 					}
 				}
-			}
 
-			// Save to file
+				// Save to file
 
-			fileName := "level.txt"
-			data := strings.Join(level, "\n")
-			file, err := os.Create(fileName)
-			if err != nil {
-				fmt.Println("Cannot create file! ", err)
-				return
-			}
-			defer file.Close()
-
-			_, err = file.WriteString(data)
-
-			if err != nil {
-				fmt.Println("Error writing to file, ", err)
-			}
-
-		}
-
-		drawSquare(&level, "  ", false, 20)
-
-		// made it only draw when needed cause computer couldn't keep up when moving the cursor fast
-		drawLevel := func() {
-			fmt.Printf("\033[s")
-			for idx, val := range level {
-				fmt.Printf("\033[%d;%dH%s", idx, 0, val)
-			}
-			fmt.Printf("\033[u")
-		}
-
-		drawLevel()
-
-		buf := make([]byte, 3)
-
-		//console := []string{}
-		//x, y := 0, 0
-		for {
-
-			//fmt.Printf("\033[%d;%dH", y, x)
-			_, err = os.Stdin.Read(buf)
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			switch buf[0] {
-			case 'q':
-				return
-			case ' ':
-				fmt.Printf("\033[s")
-				fmt.Printf("\033[47;0HCONSOLE: %s", "\033[6n")
-				fmt.Printf("\033[u")
-				drawLevel()
-			case '\033':
-				if buf[1] == '[' {
-					fmt.Printf("\033[1%s", string(buf[2]))
+				fileName := "level.txt"
+				data := strings.Join(level, "\n")
+				file, err := os.Create(fileName)
+				if err != nil {
+					fmt.Println("Cannot create file! ", err)
+					return
 				}
+				defer file.Close()
+
+				_, err = file.WriteString(data)
+
+				if err != nil {
+					fmt.Println("Error writing to file, ", err)
+				}
+
 			}
-			//console = append(console, fmt.Sprintf("X: %d  Y: %d", x, y))e
+
+			drawSquare(&level, "  ", false, 20)
+
+			// made it only draw when needed cause computer couldn't keep up when moving the cursor fast
+			drawLevel := func() {
+				fmt.Printf("\033[s")
+				for idx, val := range level {
+					fmt.Printf("\033[%d;%dH%s", idx, 0, val)
+				}
+				fmt.Printf("\033[u")
+			}
+
+			drawLevel()
+
+			buf := make([]byte, 3)
+
+			//console := []string{}
+			//x, y := 0, 0
+			for {
+
+				//fmt.Printf("\033[%d;%dH", y, x)
+				_, err = os.Stdin.Read(buf)
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				switch buf[0] {
+				case 'q':
+					return
+				case ' ':
+					fmt.Printf("\033[s")
+					fmt.Printf("\033[47;0HCONSOLE: %s", "\033[6n")
+					fmt.Printf("\033[u")
+					drawLevel()
+				case '\033':
+					if buf[1] == '[' {
+						fmt.Printf("\033[1%s", string(buf[2]))
+					}
+				}
+				//console = append(console, fmt.Sprintf("X: %d  Y: %d", x, y))e
+
+			}
 
 		}
-
 	}
 }
